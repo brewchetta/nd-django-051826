f1a977b088a29b661e5539f31013db5a0b1133da
unknown <chett.tiller@gmail.com>
2026-05-18 15:19:42 -0400

    through context

--- day-01/first_project/cat_memes_application/urls.py
+++ day-01/first_project/cat_memes_application/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.homepage),
-    path('login', views.login),
-    path('memes', views.memes)
+    path('', views.homepage, name="homepage"),
+    path('login', views.login, name="login"),
+    path('memes', views.memes, name="memes")
 ]
--- day-01/first_project/cat_memes_application/views.py
+++ day-01/first_project/cat_memes_application/views.py
@@ -9,4 +9,21 @@ def login(request):
     return render(request, "cat_memes_application/login.html")
 
 def memes(request):
-    return render(request, "cat_memes_application/memes.html")
+    cat_meme_urls = [
+        "https://upload.wikimedia.org/wikipedia/commons/d/dc/Grumpy_Cat_%2814556024763%29_%28cropped%29.jpg?utm_source=en.wikipedia.org&utm_campaign=index&utm_content=original",
+
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2v1HCs4g7fqBJMaobrYD80zJi5yx8vVNg6g&s",
+
+        "https://upload.wikimedia.org/wikipedia/en/1/1f/WomanYellingAtACat_meme.jpg",
+    ]
+
+    # context is used to pass information to the template
+    # it is a dictionary (key / value pairs)
+    context = {
+        "urls": cat_meme_urls,
+        "num_memes": len( cat_meme_urls ),
+        "something_else": "I am something else"
+    }
+
+    return render(request, "cat_memes_application/memes.html", context)
+    # context is the final optional argument in render
