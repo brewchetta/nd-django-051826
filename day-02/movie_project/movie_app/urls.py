053693dc59cd5b5c457dfe672a0ab2acc069868f
unknown <chett.tiller@gmail.com>
2026-05-19 12:42:39 -0400

    move trailers

--- day-02/movie_project/movie_app/urls.py
+++ day-02/movie_project/movie_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name="homepage"),
-    path('upcoming-movies', views.upcoming_movies, name="upcoming_movies")
+    path('upcoming-movies', views.upcoming_movies, name="upcoming_movies"),
+    path('trailer/<int:movie_index>', views.trailer, name="trailer")
 ]
--- day-02/movie_project/movie_app/views.py
+++ day-02/movie_project/movie_app/views.py
@@ -1,20 +1,33 @@
 from django.shortcuts import render
 
+movies_list = [
+    {
+        "id": 0,
+        "title": "Supergirl", 
+        "poster": "https://i.redd.it/7wgdxs5kqe6g1.jpeg", 
+        "description": "Supergirl beats up some bad dudes.",
+        "youtube_id": "s1-pfiVMKAs?si=tK7r5XJv0ZLB6mX2"
+    },
+    {
+        "id": 1,
+        "title": "Disclosure Day",
+        "poster": "https://m.media-amazon.com/images/M/MV5BMTgwNDI1ZjctYWNmMS00MTJhLTg1ZWItMzI1Yjk5NjZkYWFkXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+        "description": "Spielberg makes a movie about disclosing things about UFOs & extraterrestrials.",
+        "youtube_id": "SCYT8vb2siQ?si=GdgV02U-PuKoxgP1"
+    }
+]
+
 def homepage(request):
     return render(request, 'movie_app/homepage.html')
 
 def upcoming_movies(request):
-    movies_list = [
-        {
-            "title": "Supergirl", 
-            "poster": "https://i.redd.it/7wgdxs5kqe6g1.jpeg", 
-            "description": "Supergirl beats up some bad dudes."
-        },
-        {
-            "title": "Disclosure Day",
-            "poster": "https://m.media-amazon.com/images/M/MV5BMTgwNDI1ZjctYWNmMS00MTJhLTg1ZWItMzI1Yjk5NjZkYWFkXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
-            "description": "Spielberg makes a movie about disclosing things about UFOs & extraterrestrials."
-        }
-    ]
     context = { "movies_list": movies_list }
     return render(request, 'movie_app/upcoming_movies.html', context)
+
+def trailer(request, movie_index):
+    found_movie = movies_list[movie_index]
+    context = {
+        "title": found_movie["title"],
+        "youtube_id": found_movie["youtube_id"]
+    }
+    return render(request, 'movie_app/trailer.html', context)
