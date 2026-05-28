8ae1c9a1cf2227da6535e7bc80e54d828e34e85c
unknown <chett.tiller@gmail.com>
2026-05-28 12:23:24 -0400

    class views!

--- day-07/coffee_project/coffee_app/urls.py
+++ day-07/coffee_project/coffee_app/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('about', views.AboutView.as_view(), name='about'),
     path('clock', views.ClockView.as_view(), name='clock'),
     path('contact', views.ContactView.as_view(), name='contact'),
-    path('beverages/create', views.BeverageCreateView.as_view(), name='beverage_create')
+    # path('beverages/create', views.BeverageCreateView.as_view(), name='beverage_create'),
+    path('beverages/create', views.AlternateBeverageCreateView.as_view(), name='beverage_create'),
+    path('beverages', views.BeverageListView.as_view(), name='beverage_list'),
+    path('beverages/<int:pk>', views.BeverageDetailView.as_view(), name='beverage_detail')
 ]
--- day-07/coffee_project/coffee_app/views.py
+++ day-07/coffee_project/coffee_app/views.py
@@ -9,7 +9,6 @@ def home(request):
 
 from django.views.generic import TemplateView
 
-
 # classes like to use upper camel case
 class AboutView(TemplateView):
     template_name = 'coffee_app/about.html'
@@ -43,7 +42,7 @@ class BeverageCreateView(View):
     # this is the same as the get from the functional views
     def get(self, request):
         context = { "form": BeverageForm() }
-        return render(request, 'coffee_app/greeting.html', context)
+        return render(request, 'coffee_app/beverage_create.html', context)
 
     # POST #
     # this is the same as the `if method == 'POST'` from functional views
@@ -54,4 +53,51 @@ class BeverageCreateView(View):
             return redirect('home')
         else:
             context = { "form": form }
-            return render(request, 'coffee_app/greeting.html', context)
+            return render(request, 'coffee_app/beverage_create.html', context)
+        
+
+from django.views.generic.edit import FormView
+
+# FormView is a more specialized view that handles form submissions
+class AlternateBeverageCreateView(FormView):
+    # template to render
+    template_name = 'coffee_app/beverage_create.html'
+    # form we're using
+    form_class = BeverageForm
+    # where we go when the form is submitted correctly
+    success_url = '/'
+
+    # what happens when the form is valid
+    def form_valid(self, form):
+        form.save() # save the form
+        return super().form_valid(form) 
+        # trigger the FormView's form_valid method
+
+
+
+from django.views.generic import ListView
+from .models import Beverage
+
+# ListView will render a list page and automatically 
+# pass all the items for a model
+class BeverageListView(ListView):
+    model = Beverage
+    context_object_name = 'all_beverages'
+
+
+# CONVENTION vs CONFIGURATION
+
+# convention -- shortcuts we can use to make coding easier
+# BUT those shortcuts make it harder to do custom logic
+# and configure things in a more precise
+
+# configuration -- we do a lot of the logic and heavy lifting
+# BUT we can customize more readily
+
+
+from django.views.generic import DetailView
+
+# DetailView will take in a pk and show a page for just that item
+class BeverageDetailView(DetailView):
+    model = Beverage
+    context_object_name = 'beverage'
